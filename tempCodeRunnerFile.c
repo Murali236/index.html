302ab1f42f99292a842b003d7fae6e5369e5c21d
Murali236 <159483285+Murali236@users.noreply.github.com>
2024-02-29 23:08:41 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,R1,R2,R3,R4,R5;
+	while(t--){
+	    cin>>R1>>R2>>R3>>R4>>R5;
+	    if(R1+R2+R3+R4+R5>=4){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    cout<<endl;
+	}
+return 0;
+}
